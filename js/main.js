079ec0643cb81e12026620bbd9be24ed1019d1af
Javier-Lug0-03 <a01571142@itesm.mx>
2022-07-07 16:45:44 -0500

    Absolute reformat for mobile with 'origin/main'.

--- js/main.js
+++ js/main.js
@@ -5,12 +5,12 @@ const url4 = 'docs/Preguntas Frecuentes - Miramar 2022.pdf'
 const url5 = 'docs/pdf.pdf';
 const url6 = 'docs/F1008B Examen conocimientos.pdf'
 
+var main = document.getElementById('mainpage');
+var doc1 = document.getElementById('render1');
+var pdfbuttons = document.getElementById('pdfbuttons');
+var misc = document.getElementById('misc');
+
 //const isMobile = navigator.userAgentData.mobile; //resolves true/false
-function mobileCheck() {
-  let check = false;
-  (function(a){if(/(android|bb\d+|meego).+mobile|avantgo|bada\/|blackberry|blazer|compal|elaine|fennec|hiptop|iemobile|ip(hone|od)|iris|kindle|lge |maemo|midp|mmp|mobile.+firefox|netfront|opera m(ob|in)i|palm( os)?|phone|p(ixi|re)\/|plucker|pocket|psp|series(4|6)0|symbian|treo|up\.(browser|link)|vodafone|wap|windows ce|xda|xiino/i.test(a)||/1207|6310|6590|3gso|4thp|50[1-6]i|770s|802s|a wa|abac|ac(er|oo|s\-)|ai(ko|rn)|al(av|ca|co)|amoi|an(ex|ny|yw)|aptu|ar(ch|go)|as(te|us)|attw|au(di|\-m|r |s )|avan|be(ck|ll|nq)|bi(lb|rd)|bl(ac|az)|br(e|v)w|bumb|bw\-(n|u)|c55\/|capi|ccwa|cdm\-|cell|chtm|cldc|cmd\-|co(mp|nd)|craw|da(it|ll|ng)|dbte|dc\-s|devi|dica|dmob|do(c|p)o|ds(12|\-d)|el(49|ai)|em(l2|ul)|er(ic|k0)|esl8|ez([4-7]0|os|wa|ze)|fetc|fly(\-|_)|g1 u|g560|gene|gf\-5|g\-mo|go(\.w|od)|gr(ad|un)|haie|hcit|hd\-(m|p|t)|hei\-|hi(pt|ta)|hp( i|ip)|hs\-c|ht(c(\-| |_|a|g|p|s|t)|tp)|hu(aw|tc)|i\-(20|go|ma)|i230|iac( |\-|\/)|ibro|idea|ig01|ikom|im1k|inno|ipaq|iris|ja(t|v)a|jbro|jemu|jigs|kddi|keji|kgt( |\/)|klon|kpt |kwc\-|kyo(c|k)|le(no|xi)|lg( g|\/(k|l|u)|50|54|\-[a-w])|libw|lynx|m1\-w|m3ga|m50\/|ma(te|ui|xo)|mc(01|21|ca)|m\-cr|me(rc|ri)|mi(o8|oa|ts)|mmef|mo(01|02|bi|de|do|t(\-| |o|v)|zz)|mt(50|p1|v )|mwbp|mywa|n10[0-2]|n20[2-3]|n30(0|2)|n50(0|2|5)|n7(0(0|1)|10)|ne((c|m)\-|on|tf|wf|wg|wt)|nok(6|i)|nzph|o2im|op(ti|wv)|oran|owg1|p800|pan(a|d|t)|pdxg|pg(13|\-([1-8]|c))|phil|pire|pl(ay|uc)|pn\-2|po(ck|rt|se)|prox|psio|pt\-g|qa\-a|qc(07|12|21|32|60|\-[2-7]|i\-)|qtek|r380|r600|raks|rim9|ro(ve|zo)|s55\/|sa(ge|ma|mm|ms|ny|va)|sc(01|h\-|oo|p\-)|sdk\/|se(c(\-|0|1)|47|mc|nd|ri)|sgh\-|shar|sie(\-|m)|sk\-0|sl(45|id)|sm(al|ar|b3|it|t5)|so(ft|ny)|sp(01|h\-|v\-|v )|sy(01|mb)|t2(18|50)|t6(00|10|18)|ta(gt|lk)|tcl\-|tdg\-|tel(i|m)|tim\-|t\-mo|to(pl|sh)|ts(70|m\-|m3|m5)|tx\-9|up(\.b|g1|si)|utst|v400|v750|veri|vi(rg|te)|vk(40|5[0-3]|\-v)|vm40|voda|vulc|vx(52|53|60|61|70|80|81|83|85|98)|w3c(\-| )|webc|whit|wi(g |nc|nw)|wmlb|wonu|x700|yas\-|your|zeto|zte\-/i.test(a.substr(0,4))) check = true;})(navigator.userAgent||navigator.vendor||window.opera);
-  return check;
-};
 
 var url;
 
@@ -19,17 +19,10 @@ let pdfDoc = null,
   pageIsRendering = false,
   pageNumIsPending = null;
 
-var scale = 1.5,
+var scale = 0.5,
   canvas = document.querySelector('#pdf-render'),
   ctx = canvas.getContext('2d');
 
-if(mobileCheck() == true){
-  document.getElementById("tbdesk").hidden = true;
-  document.getElementById("mainpage").hidden = true;
-  document.getElementById("mainmob").hidden = false;
-  document.getElementById("tbmob").hidden = true;
-  scale = 0.5;
-}
 // Render the page
 const renderPage = num => {
   pageIsRendering = true;
@@ -88,33 +81,17 @@ const showNextPage = () => {
 };
 
 function mainshow(){
-  var main = document.getElementById('mainpage');
-  var doc1 = document.getElementById('render1');
-  var avisos = document.getElementById('avisos');
-  var pdfbuttons = document.getElementById('pdfbuttons');
-  var mainmob = document.getElementById('mobmain');
   main.hidden = false;
-  avisos.hidden = false;
   doc1.hidden = true;
   pdfbuttons.hidden = true;
-  if(mobileCheck() == true){
-    mainmob.hidden = false;
-  }
+  misc.hidden = true;
 }
 function pdf1(){
-  var main = document.getElementById('mainpage');
-  var doc1 = document.getElementById('render1');
-  var avisos = document.getElementById('avisos');
-  var pdfbuttons = document.getElementById('pdfbuttons');
-  var misc = document.getElementById('misc');
-  misc.hidden = true;
-
   
   main.hidden = true;
-  avisos.hidden = false;
   doc1.hidden = false;
   pdfbuttons.hidden = true;
-
+  misc.hidden = true;
   url = url1;
   
   // Get Document
@@ -143,16 +120,8 @@ function pdf1(){
 }
 
 function pdf2(){
-  var main = document.getElementById('mainpage');
-  var doc1 = document.getElementById('render1');
-  var avisos = document.getElementById('avisos');
-  var pdfbuttons = document.getElementById('pdfbuttons');
-  var misc = document.getElementById('misc');
-  misc.hidden = true;
-
-  
   main.hidden = true;
-  avisos.hidden = false;
+  misc.hidden = true;
   doc1.hidden = false;
   pdfbuttons.hidden = true;
 
@@ -183,16 +152,10 @@ function pdf2(){
   document.querySelector('#next-page').addEventListener('click', showNextPage);
 }
 function pdf3(){
-  var main = document.getElementById('mainpage');
-  var doc1 = document.getElementById('render1');
-  var avisos = document.getElementById('avisos');
-  var pdfbuttons = document.getElementById('pdfbuttons');
-  var misc = document.getElementById('misc');
   misc.hidden = true;
 
   
   main.hidden = true;
-  avisos.hidden = false;
   doc1.hidden = false;
   pdfbuttons.hidden = true;
 
@@ -223,16 +186,10 @@ function pdf3(){
   document.querySelector('#next-page').addEventListener('click', showNextPage);
 }
 function pdf4(){
-  var main = document.getElementById('mainpage');
-  var doc1 = document.getElementById('render1');
-  var avisos = document.getElementById('avisos');
-  var pdfbuttons = document.getElementById('pdfbuttons');
-  var misc = document.getElementById('misc');
   misc.hidden = true;
 
   
   main.hidden = true;
-  avisos.hidden = false;
   doc1.hidden = false;
   pdfbuttons.hidden = false;
 
@@ -263,14 +220,9 @@ function pdf4(){
   document.querySelector('#next-page').addEventListener('click', showNextPage);
 }
 function miscshow(){
-  var main = document.getElementById('mainpage');
-  var doc1 = document.getElementById('render1');
-  var avisos = document.getElementById('avisos');
-  var misc = document.getElementById('misc');
   misc.hidden = false;
   
   main.hidden = true;
-  avisos.hidden = false;
   doc1.hidden = true;
   
 
@@ -337,6 +289,33 @@ function pdfmisc(){
       document.querySelector('#prev-page').addEventListener('click', showPrevPage);
       document.querySelector('#next-page').addEventListener('click', showNextPage);
       url = url6;
+      break
+  }
+
+  
+}
+function pdfmob(){
+  var selection = document.getElementById("selector1").value;
+  var doc1 = document.getElementById('render1');
+
+  switch(selection){
+    case "def":
+      mainshow()
+      break
+    case "1":
+      pdf1()
+      break
+    case "2":
+      pdf2()
+      break
+    case "3":
+      pdf3()
+      break
+    case "4":
+      pdf4()
+      break
+    case "5":
+      miscshow()
       break
   }
 
